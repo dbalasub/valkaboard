3b7749994da5f58e69e865e61d3ac5e6f50fbcff
sridhar L <deepa.sridhar@gmail.com>
2015-03-05 08:41:05 -0800

    Associate listings to users

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -27,6 +27,7 @@ class ListingsController < ApplicationController
   # POST /listings.json
   def create
     @listing = Listing.new(listing_params)
+    @listing.user_id = current_user.id
     #if ! @listing.image.exists?
      # @listing.image = MagickTitle.say(@listing.name).filename
     #end 
