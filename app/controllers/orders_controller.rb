7eaf762f89f7924d675a6befa74effd32fda2f0e
sridhar L <deepa.sridhar@gmail.com>
2015-03-12 09:59:56 -0700

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,26 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_listing, only: [:new, :create, :show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_filter :check_buyer, only: [:edit, :update, :destroy, :create]
 
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
+  def sales
+    @orders =  Order.all.where(seller: current_user).order("created_at DESC")
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
+  def purchases
+    @orders =Order.all.where(buyer: current_user).order("created_at DESC")
   end
+
+  
 
   # GET /orders/new
   def new
     @order = Order.new
-    @listing = Listing.find(params[:listing_id])
+    #@listing = Listing.find(params[:listing_id])
   end
 
-  # GET /orders/1/edit
-  def edit
-  end
+  
 
   # POST /orders
   # POST /orders.json
@@ -29,7 +28,7 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.buyer_id = current_user.id
     
-    @listing = Listing.find(params[:listing_id])
+    #@listing = Listing.find(params[:listing_id])
 
     @seller = @listing.user
     @order.listing_id = @listing.id
@@ -60,24 +59,23 @@ class OrdersController < ApplicationController
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+      @listing = Listing.find(params[:listing_id])
+    end
     def set_order
       @order = Order.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:address, :city, :state)
+      params.require(:order).permit(:address, :city, :state, :listing_id)
+    end
+    def check_buyer
+      if current_user == @listing.user
+        redirect_to root_url, alert: "Sorry, can't buy your own listing"
+      end
     end
 end
